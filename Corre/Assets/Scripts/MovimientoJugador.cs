90e34a77d24db1a6d37ca68c1891dae503cc5c48
PACHECO 2099 <122486806+RicardoSantana2099@users.noreply.github.com>
2024-08-04 23:24:55 -0600

    CualquierTecla
    Se modifico el script del jugador para que se mueva con cualquier tecla que se presione

--- Corre/Assets/Scripts/MovimientoJugador.cs
+++ Corre/Assets/Scripts/MovimientoJugador.cs
@@ -5,42 +5,25 @@ using UnityEngine;
 public class MovimientoJugador : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float jumpForce = 5f;
-    public bool isGrunded;
 
     private Rigidbody2D rb;
-    private Collider2D coll;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        coll = GetComponent<Collider2D>();
     }
 
     
     void Update()
     {
-        float moveImput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(moveImput * moveSpeed, rb.velocity.y);
-
-        if (isGrunded && Input.GetKeyDown(KeyCode.Space))
+       if(Input.anyKey)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.CompareTag("Ground"))
-        {
-            isGrunded = true;
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if(collision.gameObject.CompareTag("Ground"))
-        {
-            isGrunded = false;
-        }
-    }
 }
